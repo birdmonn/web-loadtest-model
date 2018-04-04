2ccf019a55f233c29bfb9be1d96d0e4bfc1c9a16
birdmonn <girapat.bird@gmail.com>
2018-04-04 10:38:45 +0700

    inti

---
+++ src/main/java/com/example/rif/RifApplication.java
@@ -0,0 +1,12 @@
+package com.example.rif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RifApplication.class, args);
+	}
+}
