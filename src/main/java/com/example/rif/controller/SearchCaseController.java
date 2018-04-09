dfc743d50e2f9183aae837921b48bf4ebfffffb5
birdmonn <girapat.bird@gmail.com>
2018-04-09 15:46:15 +0700

    add table fix css

--- src/main/java/com/example/rif/controller/SearchCaseController.java
+++ src/main/java/com/example/rif/controller/SearchCaseController.java
@@ -1,9 +1,24 @@
 package com.example.rif.controller;
 
+import com.example.rif.service.RequestFormService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/form")
+@RequestMapping("/search-case")
 public class SearchCaseController {
+    private RequestFormService requestFormService;
+
+    @Autowired
+    public SearchCaseController(RequestFormService requestFormService) {
+        this.requestFormService = requestFormService;
+    }
+    @GetMapping()
+    public String list(Model model){
+        model.addAttribute("caseList", requestFormService.findall());
+        return "search-case";
+    }
 }
